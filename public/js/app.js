eed2860a2ccd06e53b400f438fde569e953e075a
Francis John <francis.john@colorado.edu>
2015-02-14 17:36:55 -0800

    buggy mvp complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,91 @@
 var visualizer = (function(){
-  var cube, current_base, renderer, scene, camera, dataArray, analyzer, rms, randomColors, prevScale, scale; 
-
+  var cube, current_base, renderer, scene, camera, dataArray, analyzer, rms, randomColors, prevScale, scale, currTrackId, client_id, lastTime, stats; 
   function init(){
+    addLoadingScreen();
+    bindEvents();
+    setupStats();
     setupObjects();
     setupAudio();
     setupCamera();
     render();
   }
 
-  function render() {
-    requestAnimationFrame( render );
-    animate();
+  function render(time) {
+    stats.begin();
+    animate( time);
     renderer.render( scene, camera );
+    stats.end();
+    requestAnimationFrame( render );
+  }
+
+  function bindEvents(){    
+    $("#searchContainer").submit(function(evt){
+        evt.preventDefault();
+        console.log($("#searchList li").first().attr("id"))
+       if($("#searchList li").length > 0)
+       {
+        currTrackId = $("#searchList li").first().attr("id");
+        loadSong();
+       }
+       else
+        newSearch();
+    })
+    $("#searchList").on("mousedown", "li", function(evt){
+      currTrackId = $(this).attr("id");
+      loadSong();
+      $("#searchList").slideUp();
+      $("#searchList li").remove();
+    });
+    $("#search").blur(function(){
+      $("#search").val("")
+      $("#searchList").slideUp();
+      $("#searchList li").remove();
+    });
+
+    $("#search").on("keyup", function(){
+      console.log('hitting keypress?')
+      if($(this).val().length > 5){
+        console.log('researching?')
+        newSearch();
+      }
+    });
+  }
+
+  function setupStats(){
+    stats = new Stats();
+    stats.setMode(1); // 0: fps, 1: ms
+
+    // align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.right = '0px';
+    stats.domElement.style.bottom = '0px';
+    document.body.appendChild( stats.domElement );
+  }
+  function newSearch(){
+    var count = 0;
+    $.get('http://api.soundcloud.com/tracks/?client_id=' + client_id, {q: $("#search").val()})
+     .done(function(response){
+      console.log(response)
+      $("#searchList li").remove();
+      console.log(response)
+      for(var i = 0; i < response.length; i++)
+      {
+        if(response[i].streamable === true)
+        {
+          $("#searchList").append("<li id=" + response[i].id + ">" + response[i].title + "</li>")
+          count += 1;
+        }
+        if(count > 10)
+          break;
+      }
+      $("#searchList").slideDown();
+    }).fail(function(response, what){
+      console.log(response)
+      console.log(what)
+    })
   }
 
   function randomizeColors(){
-    console.log('wtf')
     if (current_base.getHex().toString(16) === "ffffff" || current_base.getHex().toString(16) === "000000")
       current_base.setHex(Math.random()*0xFFFFFF);
     else
@@ -23,21 +93,21 @@ var visualizer = (function(){
       current_base.set('#' + (current_base.getHex() + 100101).toString(16));
     }
       cube.material.specular.set('#' + (cube.material.specular.getHex() + 1000).toString(16));
-    console.log(current_base)
-
   } 
 
   function volScale(val, poss){
     return ((rms - 120)/ 50)*(val / poss)
   }
-  function animate(){
+  function animate(time){
+    TweenMax.to(camera.position, 0.5, {x: (mouse.x*50 - camera.position.x), y: (mouse.y*50 - camera.position.y)})
+    camera.lookAt( scene.position );
+    console.log();
     analyzer.getByteTimeDomainData(dataArray);
     rms = helpers.rootMeanSquare(dataArray);
-    console.log(rms)
     prevScale = scale; 
     scale = volScale(1, 1);
-    if(Math.abs(prevScale - scale) > 0.03)
-      TweenMax.to(cube.scale, 0.5, {x: scale*5, y: scale*5, z: scale*5});
+    if(Math.abs(prevScale - scale) > 0.05)
+      TweenMax.to(cube.scale, 0.3, {x: scale*5, y: scale*5, z: scale*5});
     if (rms > 150)
       randomColors();
     TweenMax.to(cube.material.color, 0.5, {r: volScale(current_base.r, 0.8), g: (volScale(current_base.g, 0.8)), b: (volScale(current_base.b, 0.8))});
@@ -48,9 +118,13 @@ var visualizer = (function(){
 
   function setupObjects(){
     renderer = new THREE.WebGLRenderer();
+    $("body").height(window.innerHeight);
+    $("body").width(window.innerWidth);
     renderer.setSize( window.innerWidth, window.innerHeight );
     $("#canvas").append( $(renderer.domElement) );
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.x = 0;
+    camera.position.y = 0;
     camera.position.z = 5;
     current_base = new THREE.Color(0xffffff)
     randomColors = helpers.throttle(randomizeColors, 1000)
@@ -77,9 +151,12 @@ var visualizer = (function(){
   }
 
   function setupAudio(){
+    currTrackId = '63121479';
+    client_id = '74b881a5de59c54912d700542b60b955'
     var context = new AudioContext();
     analyzer = context.createAnalyser();
-    var song = $("#song")[0]
+    var song = $("#song")[0];
+    loadSong();
     var source = context.createMediaElementSource(song);
     dataArray = new Uint8Array(analyzer.frequencyBinCount)
     source.connect(context.destination)
@@ -87,17 +164,39 @@ var visualizer = (function(){
     analyzer.getByteTimeDomainData(dataArray);
     song.play();
   }
-
-  function setupCamera(){   
-    var mouse = {x : 0, y : 0};
-    $("canvas").on('mousedown', function(event){
-      var xVal = event.clientX > window.innerWidth/2 ? 0.5 : -0.5
-      var yVal = event.clientY > window.innerHeight/2 ? 0.5 : -0.5
-
-      TweenMax.to(camera.position, 0.1, {x: camera.position.x + xVal , y: camera.position.y + yVal})
-      camera.lookAt( scene.position )
+  function loadSong(){
+    addLoadingScreen();
+    song.src = 'http://api.soundcloud.com/tracks/' + currTrackId + '/stream?client_id=' + client_id;
+    $.get('http://api.soundcloud.com/tracks/' + currTrackId + '.json?client_id=' + client_id, function(songData){
+      $("#songArtwork").attr("src",songData.artwork_url)
+      $("#currSong").text(songData.title)
+      $("#currSong").attr("href", songData.permalink_url)
+      removeLoadingScreen();
+      song.play();
+      var min = parseInt(songData.duration / 60000);
+      var sec = parseInt((songData.duration / 1000) % 60);
+      sec = sec < 10 ? '0' + sec : sec;
+      console.log(song.curr)
+      console.log(min)
+      $("#duration").text(min + ":" + sec)
     })
   }
+  var mouse = {x : 0, y : 0};
+  function setupCamera(){   
+    $("canvas").on('mousemove', function(event){
+      mouse.x = (event.clientX / window.innerWidth) - 0.5;
+      mouse.y = (event.clientY / window.innerHeight) - 0.5;
+    })
+  }
+  function addLoadingScreen(){
+    $("#canvas").addClass("blur");
+    $("#loadingAnimation").show();
+  }
+  function removeLoadingScreen(){
+    $("#loadingAnimation").hide();
+    $("#canvas").removeClass("blur");
+  }
+
   return {
     init: init 
   }
