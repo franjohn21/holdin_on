902c912f85b7eea2461fc732c177e7890d9de153
Francis John <francis.john@colorado.edu>
2015-02-15 12:20:00 -0800

    Add frequency bars

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,10 @@
 var visualizer = (function(){
-  var cube, current_base, renderer, scene, camera, dataArray, analyzer, rms, randomColors, prevScale, scale, currTrackId, client_id, lastTime, stats; 
+  var cube, current_base, renderer, scene, camera, dataArray, analyzer, rms, randomColors, prevScale, scale, currTrackId, client_id, lastTime, stats, bars, controls; 
   function init(){
-    addLoadingScreen();
     bindEvents();
     setupStats();
-    setupObjects();
     setupAudio();
+    setupObjects();
     setupCamera();
     render();
   }
@@ -43,9 +42,7 @@ var visualizer = (function(){
     });
 
     $("#search").on("keyup", function(){
-      console.log('hitting keypress?')
       if($(this).val().length > 5){
-        console.log('researching?')
         newSearch();
       }
     });
@@ -92,27 +89,38 @@ var visualizer = (function(){
     {
       current_base.set('#' + (current_base.getHex() + 100101).toString(16));
     }
-      cube.material.specular.set('#' + (cube.material.specular.getHex() + 1000).toString(16));
+    for(var i = 0; i < dataArray.length; i++){ 
+      bars[i].material.specular.set('#' + (bars[i].material.specular.getHex() + 1000).toString(16));
+    }
   } 
 
   function volScale(val, poss){
     return ((rms - 120)/ 50)*(val / poss)
   }
   function animate(time){
-    TweenMax.to(camera.position, 0.5, {x: (mouse.x*50 - camera.position.x), y: (mouse.y*50 - camera.position.y)})
-    camera.lookAt( scene.position );
-    console.log();
+    // TweenMax.to(camera.position, 0.5, {x: (mouse.x*200 - camera.position.x), y: (mouse.y*200 - camera.position.y)})
+    // camera.lookAt(scene.position)
     analyzer.getByteTimeDomainData(dataArray);
     rms = helpers.rootMeanSquare(dataArray);
+    controls.update();
+
     prevScale = scale; 
     scale = volScale(1, 1);
-    if(Math.abs(prevScale - scale) > 0.05)
-      TweenMax.to(cube.scale, 0.3, {x: scale*5, y: scale*5, z: scale*5});
+    // if(Math.abs(prevScale - scale) > 0.05)
+      // TweenMax.to(cube.scale, 0.3, {x: scale*5, y: scale*5, z: scale*5});
     if (rms > 150)
       randomColors();
-    TweenMax.to(cube.material.color, 0.5, {r: volScale(current_base.r, 0.8), g: (volScale(current_base.g, 0.8)), b: (volScale(current_base.b, 0.8))});
-    cube.rotation.y += 0.001;
-    cube.rotation.x += 0.001;
+    // TweenMax.to(cube.material.color, 0.5, {r: volScale(current_base.r, 0.8), g: (volScale(current_base.g, 0.8)), b: (volScale(current_base.b, 0.8))});
+    // cube.rotation.y += 0.001;
+    // cube.rotation.x += 0.001;
+    for(var i = 0; i < dataArray.length; i++){
+      TweenMax.to(bars[i].material.color, 0.32, {r: volScale(current_base.r, 0.8), g: (volScale(current_base.g, 0.8)), b: (volScale(current_base.b, 0.8))});
+      if(Math.abs(bars[i].scale.y - (dataArray[i]/128.0)*50) > 0.001){
+        TweenMax.to(bars[i].scale, 0.3, {y: (dataArray[i]/128.0)*10, z: volScale(3, 1)})
+      }
+      else
+        TweenMax.to(bars[i].scale, 0.3, {z: volScale(10, 1)})
+    }
   }
 
 
@@ -124,27 +132,47 @@ var visualizer = (function(){
     $("#canvas").append( $(renderer.domElement) );
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 5;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    controls = new THREE.TrackballControls( camera );
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+
+    controls.noZoom = false;
+    controls.noPan = false;
+
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
     current_base = new THREE.Color(0xffffff)
     randomColors = helpers.throttle(randomizeColors, 1000)
-    var geometry = new THREE.BoxGeometry( 1, 1, 1);
-    var material = new THREE.MeshPhongMaterial( { color: current_base, specular: 0x808080, shininess: 3} );
-    cube = new THREE.Mesh( geometry, material );
-    cube.rotation.y += 0.5;
-    cube.rotation.x += 0.5;
+    scene = new THREE.Scene();
+
+    bars = [];
+    for(var i = 0; i < dataArray.length; i++)
+    {
+      var geometry = new THREE.BoxGeometry( .1, .5, 1);
+      var material = new THREE.MeshPhongMaterial( { color: current_base, specular: 0x808080, shininess: 3} );
+      bars[i] = new THREE.Mesh( geometry, material );
+      bars[i].position.set((i-dataArray.length/2)/10.0, 0, 0);
+      scene.add(bars[i]);
+
+    }
+
+
+    // var geometry = new THREE.BoxGeometry( 1, 1, 1);
+    // var material = new THREE.MeshPhongMaterial( { color: current_base, specular: 0x808080, shininess: 3} );
+    // cube = new THREE.Mesh( geometry, material );
     var directionalLight = new THREE.DirectionalLight(0xF0F0F0);
     directionalLight.position.set(1, 1, 1).normalize();
     var ambientLight = new THREE.AmbientLight(0x888888);
     var stars = new THREE.Mesh(
-      new THREE.SphereGeometry(64, 32, 32), 
+      new THREE.SphereGeometry(64, 100, 100), 
       new THREE.MeshBasicMaterial({
         map: THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'), 
         side: THREE.BackSide
       })
     );
-    scene = new THREE.Scene();
-    scene.add(cube);
     scene.add(directionalLight);
     scene.add(ambientLight);
     scene.add(stars);
@@ -162,22 +190,19 @@ var visualizer = (function(){
     source.connect(context.destination)
     source.connect(analyzer);
     analyzer.getByteTimeDomainData(dataArray);
-    song.play();
   }
   function loadSong(){
     addLoadingScreen();
-    song.src = 'https://api.soundcloud.com/tracks/' + currTrackId + '/stream?client_id=' + client_id;
     $.get('https://api.soundcloud.com/tracks/' + currTrackId + '.json?client_id=' + client_id, function(songData){
       $("#songArtwork").attr("src",songData.artwork_url)
       $("#currSong").text(songData.title)
       $("#currSong").attr("href", songData.permalink_url)
       removeLoadingScreen();
+      song.src = 'https://api.soundcloud.com/tracks/' + currTrackId + '/stream?client_id=' + client_id;
       song.play();
       var min = parseInt(songData.duration / 60000);
       var sec = parseInt((songData.duration / 1000) % 60);
       sec = sec < 10 ? '0' + sec : sec;
-      console.log(song.curr)
-      console.log(min)
       $("#duration").text(min + ":" + sec)
     })
   }
